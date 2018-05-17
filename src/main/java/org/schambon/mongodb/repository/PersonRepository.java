0b2d348f3e59640d514354cec585ec7aca180d3a
Sylvain Chambon <sylvain.chambon@mongodb.com>
2018-05-17 16:14:28 +0200

    Use of Document class (commented out)

--- src/main/java/org/schambon/mongodb/repository/PersonRepository.java
+++ src/main/java/org/schambon/mongodb/repository/PersonRepository.java
@@ -1,12 +1,16 @@
 package org.schambon.mongodb.repository;
 
 import com.mongodb.MongoClient;
+import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.schambon.mongodb.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,8 +23,9 @@ public class PersonRepository {
     @Autowired
     MongoClient mongoClient;
 
-    public Person createNew(Person person) {
+    public Person createNew(Person person) throws ParseException {
         MongoCollection<Person> people = mongoClient.getDatabase("app").getCollection("people", Person.class);
+//        MongoCollection<Document> peopleAsDocument = mongoClient.getDatabase("app").getCollection("people");
 
         // generate an _id (of type ObjectId) for the Person
         person.setId(new ObjectId());
@@ -28,6 +33,16 @@ public class PersonRepository {
         //created now
         person.setCreated(new Date());
         people.insertOne(person);
+
+//        Document doc = Document.parse("{'hello':'world', 'date':'2018-01-01T00:00:00'}");
+//        doc.put("_id", new ObjectId());
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-mm-DDTHH:MM:SS");
+//
+//        doc.put("date", dateFormat.parse(doc.getString("date")));
+
+
+//        peopleAsDocument.insertOne(doc);
+
 
         return person; // or throw a RuntimeException
     }
